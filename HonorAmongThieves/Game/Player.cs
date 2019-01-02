92e9b55c23bf9b38f7d15168fa43b2288bf6c51f
AntSharK <great_ant@hotmail.com>
2019-01-02 03:10:36 -0800

    A bunch of TODOs for potential messages

--- HonorAmongThieves/Game/Player.cs
+++ HonorAmongThieves/Game/Player.cs
@@ -73,122 +73,162 @@ namespace HonorAmongThieves.Game
         }
 
         public void GenerateFateMessage(bool heistHappens, bool policeReported, List<Player> heisters)
-        {
-            this.Decision.FateTitle = "TODO";
-            this.Decision.FateDescription = "TODO";
-            //// TODO: Resolve blackmailing.
-            //if (this.Decision.NextStatus == Status.Dead)
-            //{
-            //    var message = TextGenerator.DeathMessage(this);
-            //    this.Decision.FateTitle = this.Decision.FateTitle + message.Item1;
-            //    this.Decision.FateDescription = this.Decision.FateDescription + message.Item2;
-            //}
-            //
-            //// TODO: Handle case where successful murder doesn't leave 
-            //if (this.Decision.NextStatus != Status.Dead
-            //    && this.Decision.Killers?.Count > 0)
-            //{
-            //    var message = TextGenerator.DeathMessage(this);
-            //    this.Decision.FateTitle = this.Decision.FateTitle + message.Item1;
-            //    this.Decision.FateDescription = this.Decision.FateDescription + message.Item2;
-            //}
-            //
-            //if (this.Decision.ReportPolice)
-            //{
-            //    if (!this.Decision.HeistHappens)
-            //    {
-            //        this.Decision.FateTitle = this.Decision.FateTitle + "ARRESTED FOR MISLEADING POLICE";
-            //        this.Decision.FateDescription = this.Decision.FateDescription + "You reported a heist to the police, but the heist didn't happen. So you got arrested for wasting their time. You also got fined for $" + -this.Decision.NetworthChange + " MILLION.";
-            //        return;
-            //    }
-            //    else
-            //    {
-            //        if (this.Decision.GoOnHeist)
-            //        {
-            //            this.Decision.FateTitle = this.Decision.FateTitle + "ARRESTED WITH REDUCED PENALTY";
-            //            this.Decision.FateDescription = this.Decision.FateDescription + "You went on the heist, but also reported the details to the police. They threw you and your heistmates in jail, and rewarded you with $" + this.Decision.NetworthChange + " MILLION.";
-            //            return;
-            //        }
-            //        else
-            //        {
-            //            this.Decision.FateTitle = this.Decision.FateTitle + "SUCCESSFULLY SNITCHED";
-            //            this.Decision.FateDescription = this.Decision.FateDescription + "You stalked your heistmates as they committing the heist, and reported the details to the police. They threw your heistmates in jail, and rewarded you for $" + this.Decision.NetworthChange + " MILLION.";
-            //            return;
-            //        }
-            //    }
-            //}
-            //
-            //if (this.Decision.PlayerToKill != null
-            //    && this.Decision.PlayerToKill.Decision.NextStatus == Status.Dead)
-            //{
-            //    this.Decision.FateTitle = this.Decision.FateTitle + "KILLED " + this.Decision.PlayerToKill.Name + ", ";
-            //    if (this.Decision.PlayerToKill.Decision.Killers.Count > 1)
-            //    {
-            //        this.Decision.FateDescription = this.Decision.FateDescription + "You found " + this.Decision.PlayerToKill.Name + " and snuck up behind him. But before you could kill him, a shot rang out and he fell to the floor. You swung by later to check that he was really date, and stole some of his credit cards. ";
-            //    }
-            //    else
-            //    {
-            //        this.Decision.FateDescription = this.Decision.FateDescription + "You found " + this.Decision.PlayerToKill.Name + " and put a bullet between his eyes. Then you swiftly emptied his bank account into yours. ";
-            //    }
-            //}
-            //else if (this.Decision.PlayerToKill != null
-            //    && this.Decision.PlayerToKill.Decision.NextStatus != Status.Dead
-            //    && !this.Decision.PlayerToKill.Decision.GoOnHeist)
-            //{
-            //    this.Decision.FateTitle = this.Decision.FateTitle + "COULD NOT FIND " + this.Decision.PlayerToKill.Name + ", ";
-            //    this.Decision.FateDescription = this.Decision.FateDescription + "Your contacts found " + this.Decision.PlayerToKill.Name + " halfway around the world, and you decide murder is too troublesome this time. ";
-            //}
-            //
-            //if (this.Decision.GoOnHeist)
-            //{
-            //    if (!this.Decision.HeistHappens)
-            //    {
-            //        this.Decision.FateTitle = this.Decision.FateTitle + "NOT ENOUGH ATTENDENCE";
-            //        this.Decision.FateDescription = this.Decision.FateDescription + "You gather for the heist, but there aren't enough people here to safely complete the job. So you all go home without trying. Your total networth change this year: $" + this.Decision.NetworthChange + " MILLION.";
-            //    }
-            //    else
-            //    {
-            //        if (this.Decision.PoliceReported)
-            //        {
-            //            this.Decision.FateTitle = this.Decision.FateTitle + "ARRESTED";
-            //            this.Decision.FateDescription = this.Decision.FateDescription + "You are arrested during the heist! You are sent to jail and fined! Your total networth change this year: " 
-            //                + (this.Decision.NetworthChange > 0 ? ("$" + this.Decision.NetworthChange) : ("-$" + -this.Decision.NetworthChange)) + " MILLION.";
-            //        }
-            //        else
-            //        {
-            //            this.Decision.FateTitle = this.Decision.FateTitle + "SUCCESSFUL HEIST";
-            //            this.Decision.FateDescription = this.Decision.FateDescription + "Successful heist! Your total networth change this year: $" + this.Decision.NetworthChange + " MILLION.";
-            //        }
-            //    }
-            //}
-            //
-            //if (this.Decision.KillFailure
-            //    && this.Room.SnitchMurderWindow >= 0
-            //    && !this.Decision.PoliceReported)
-            //{
-            //    // If we reach this point, that means that failure to kill results in jail, not death
-            //    this.Decision.FateTitle = this.Decision.FateTitle + " AND LEFT OUT TO DRY";
-            //    this.Decision.FateDescription = this.Decision.FateDescription + " You confronted " + this.Decision.PlayerToKill.Name + ". Things got heated, you were left on the floor bleeding, for the cops to arrest.";
-            //    return;
-            //}
-            //
-            //else if (this.Decision.KillFailure
-            //    && this.Room.SnitchMurderWindow >= 0
-            //    && !this.Decision.PoliceReported)
-            //{
-            //    // If we reach this point, that means that failure to kill results in jail, not death
-            //    this.Decision.FateTitle = this.Decision.FateTitle + " AND FAILED TO KILL";
-            //    this.Decision.FateDescription = this.Decision.FateDescription + " You confronted " + this.Decision.PlayerToKill.Name + " as you were being arrested, but your accusations were refuted.";
-            //    return;
-            //}
-            //
-            //if (!this.Decision.GoOnHeist
-            //    && !this.Decision.ReportPolice)
-            //{
-            //    this.Decision.FateTitle = "TAKING A VACATION";
-            //    this.Decision.FateDescription = "You spend the year doing other fun exciting things. Much like crime, but without the downside of getting arrested.";
-            //    return;
-            //}
+        {            
+            // Resolve blackmail not finding the target
+            if (this.Decision.ExtortionSuccessful == null
+                && this.Decision.PlayerToBlackmail != null)
+            {
+                // TODO: MESSAGE FOR BLACKMAILED ON VACATION
+            }
+
+            // Resolve blackmailing a snitch before he snitched and doesn't go on a heist
+            if (this.Decision.ExtortionSuccessful.HasValue
+                && this.Decision.ExtortionSuccessful.Value
+                && !this.Decision.PlayerToBlackmail.Decision.GoOnHeist
+                && this.Decision.PlayerToBlackmail.Decision.ReportPolice)
+            {
+                // TODO: MESSAGE FOR EXTRACTING MONEY FROM SNITCH AND STOPPING HIM FROM SNITCHING
+            }
+
+            // Resolve heist status
+            if (this.Decision.GoOnHeist)
+            {
+                if (!heistHappens)
+                {
+                    // TODO: MESSAGE FOR HEIST NOT HAPPENING
+                }
+                else if (heistHappens
+                    && policeReported)
+                {
+                    // TODO: MESSAGE FOR GETTING ARRESTED
+                }
+            }
+
+            // Resolve snitching status - only for when snitching is successful and the snitch isn't blackmailed
+            if (this.Decision.ReportPolice
+                && policeReported
+                && (!this.Decision.WasExtortedFrom.HasValue || this.Decision.WasExtortedFrom.Value))
+            {
+                if (this.Decision.GoOnHeist
+                    && !heistHappens)
+                {
+                    // TODO: MESSAGE FOR BEING ARRESTED EVEN THOUGH HEIST DOESN'T HAPPEN
+                }
+                else if (this.Decision.GoOnHeist
+                    && heistHappens)
+                {
+                    // TODO: MESSAGE FOR BEING ARRESTED WHEN HEIST HAPPENS AND YOU WENT
+                }
+                else if (!this.Decision.GoOnHeist
+                    && heistHappens)
+                {
+                    // TODO: MESSAGE FOR POLICE REWARD
+                }
+                else if (!this.Decision.GoOnHeist
+                    && !heistHappens)
+                {
+                    // TODO: MESSAGE FOR FALSE REPORTING
+                }
+            }
+
+            // Resolve blackmailing others who went on the heist
+            if (this.Decision.PlayerToBlackmail != null
+                && this.Decision.PlayerToBlackmail.Decision.GoOnHeist)
+            {
+                if (this.Decision.ExtortionSuccessful.HasValue 
+                    && this.Decision.ExtortionSuccessful.Value)
+                {
+                    if (heistHappens
+                        && policeReported)
+                    {
+                        // TODO: MESSAGE FOR SUCCESSFUL EXTORTION AFTER HEIST IS REPORTED
+                    }
+                    else if (heistHappens
+                        && !policeReported)
+                    {
+                        // TODO: MESSAGE FOR SUCCESSFUL EXTORTION AFTER HEIST HAPPENS
+                    }
+                    else if (!heistHappens)
+                    {
+                        // TODO: MESSAGE FOR SUCCESSFUL EXTORTION AFTER DISBANDING
+                    }
+                }
+                else if (this.Decision.ExtortionSuccessful.HasValue
+                    && !this.Decision.ExtortionSuccessful.Value)
+                {
+                    if (heistHappens
+                        && policeReported)
+                    {
+                        // TODO: MESSAGE FOR FAILED EXTORTION AFTER HEIST IS REPORTED
+                    }
+                    else if (heistHappens
+                        && !policeReported)
+                    {
+                        // TODO: MESSAGE FOR FAILED EXTORTION AFTER HEIST HAPPENS
+                    }
+                    else if (!heistHappens)
+                    {
+                        // TODO: MESSAGE FOR FAILED EXTORTION AFTER DISBANDING
+                    }
+                }
+            }
+
+            // Resolve getting blackmailed and going on a heist
+            if (this.Decision.Blackmailers != null
+                && this.Decision.GoOnHeist)
+            {
+                if (this.Decision.WasExtortedFrom.HasValue
+                    && this.Decision.WasExtortedFrom.Value)
+                {
+                    if (heistHappens
+                        && policeReported)
+                    {
+                        // TODO: MESSAGE FOR GETTING BLACKMAILED AFTER HEIST IS REPORTED
+                    }
+                    else if (heistHappens
+                        && !policeReported)
+                    {
+                        // TODO: MESSAGE FOR GETTING BLACKMAILED AFTER HEIST HAPPENS
+                    }
+                    else if (!heistHappens)
+                    {
+                        // TODO: MESSAGE FOR GETTING BLACKMAILED AFTER DISBANDING
+                    }
+                }
+                else if (this.Decision.WasExtortedFrom.HasValue
+                    && !this.Decision.WasExtortedFrom.Value)
+                {
+                    if (heistHappens
+                        && policeReported)
+                    {
+                        // TODO: MESSAGE FOR DEFENDING SELF AFTER HEIST IS REPORTED
+                    }
+                    else if (heistHappens
+                        && !policeReported)
+                    {
+                        // TODO: MESSAGE FOR DEFENDING SELF AFTER HEIST HAPPENS
+                    }
+                    else if (!heistHappens)
+                    {
+                        // TODO: MESSAGE FOR DEFENDING SELF AFTER DISBANDING
+                    }
+                }
+            }
+
+            // Resolve not going on the heist but getting blackmailed while snitching
+            if (this.Decision.WasExtortedFrom.HasValue
+                && this.Decision.GoOnHeist
+                && this.Decision.WasExtortedFrom.Value)
+            {
+                // TODO: MESSAGE FOR GETTING BLACKMAILED WHILE SNITCHING BUT NOT ON HEIST
+            }
+
+            // Resolve doing nothing
+            if (!this.Decision.GoOnHeist
+                && !this.Decision.ReportPolice)
+            {
+                // TODO: MESSAGE FOR BAILING
+            }
         }
 
         public async Task ResumePlayerSession(HeistHub hub)
