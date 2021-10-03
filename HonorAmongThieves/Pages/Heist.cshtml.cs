2c775d82cc1fc2a55543941b4f6ece60dd283e29
Anthony Moh <great_ant@hotmail.com>
2021-10-03 16:12:45 -0700

    Put instructions and room list in game lobby

--- HonorAmongThieves/Pages/Heist.cshtml.cs
+++ HonorAmongThieves/Pages/Heist.cshtml.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System;
+using System.Collections.Generic;
+using HonorAmongThieves.Heist.GameLogic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace HonorAmongThieves.Pages
 {
     public class HeistModel : PageModel
     {
-        public void OnGet()
-        {
+        public List<Tuple<string, DateTime, int, string>> HeistRooms;
 
+        public void OnGet(
+            [FromServices] HeistGame heistLobby)
+        {
+            HeistRooms = new List<Tuple<string, DateTime, int, string>>();
+            foreach (var room in heistLobby.Rooms.Values)
+            {
+                HeistRooms.Add(Tuple.Create(room.Id, room.UpdatedTime, room.Players.Count, room.SettingUp ? "SIGNING UP" : "STARTED"));
+            }
         }
     }
 }
