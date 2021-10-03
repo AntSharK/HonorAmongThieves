f1767a29890ac53d8cfbd894af6e4b03897df93f
Anthony Moh <great_ant@hotmail.com>
2021-10-03 16:18:34 -0700

    Extract out cakery lobby

--- HonorAmongThieves/Pages/Cakery.cshtml.cs
+++ HonorAmongThieves/Pages/Cakery.cshtml.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System;
+using System.Collections.Generic;
+using HonorAmongThieves.Cakery.GameLogic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace HonorAmongThieves.Pages
 {
     public class CakeryModel : PageModel
     {
-        public void OnGet()
-        {
+        public List<Tuple<string, DateTime, int, string>> CakeryRooms;
 
+        public void OnGet(
+            [FromServices] CakeryGame cakeryLobby)
+        {
+            CakeryRooms = new List<Tuple<string, DateTime, int, string>>();
+            foreach (var room in cakeryLobby.Rooms.Values)
+            {
+                CakeryRooms.Add(Tuple.Create(room.Id, room.UpdatedTime, room.Players.Count, room.SettingUp ? "SIGNING UP" : "STARTED"));
+            }
         }
     }
 }
--- HonorAmongThieves/Pages/Index.cshtml.cs
+++ HonorAmongThieves/Pages/Index.cshtml.cs
@@ -1,32 +1,11 @@
-using System;
-using System.Collections.Generic;
-using HonorAmongThieves.Cakery.GameLogic;
-using HonorAmongThieves.Heist.GameLogic;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace HonorAmongThieves.Pages
 {
     public class IndexModel : PageModel
     {
-        public List<Tuple<string, DateTime, int, string>> HeistRooms;
-        public List<Tuple<string, DateTime, int, string>> CakeryRooms;
-
-        public void OnGet(
-            [FromServices] HeistGame heistLobby,
-            [FromServices] CakeryGame cakeryLobby)
+        public void OnGet()
         {
-            HeistRooms = new List<Tuple<string, DateTime, int, string>>();
-            foreach (var room in heistLobby.Rooms.Values)
-            {
-                HeistRooms.Add(Tuple.Create(room.Id, room.UpdatedTime, room.Players.Count, room.SettingUp ? "SIGNING UP" : "STARTED"));
-            }
-
-            CakeryRooms = new List<Tuple<string, DateTime, int, string>>();
-            foreach (var room in cakeryLobby.Rooms.Values)
-            {
-                CakeryRooms.Add(Tuple.Create(room.Id, room.UpdatedTime, room.Players.Count, room.SettingUp ? "SIGNING UP" : "STARTED"));
-            }
         }
     }
 }
